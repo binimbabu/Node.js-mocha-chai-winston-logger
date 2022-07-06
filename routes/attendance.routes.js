ff3038fb7f3b5052cef14ead5a37dbec0b51e322
Bini M Babu <binibabu22@gmail.com>
2022-07-06 13:32:43 +0530

    Error Route.post fixed

--- routes/attendance.routes.js
+++ routes/attendance.routes.js
@@ -2,7 +2,8 @@ module.exports = app => {
     const attendance = require("../handlers/attendance.controller");
     var router = require("express").Router();
     // Create a new Tutorial
-    router.post("/", attendance.create);
+    router.post("/" , function(req, res){
+      attendance.create});
     router.get('/attend', attendance.employeeListAttendence);
     router.get('/view', attendance.employeeViewAttendence);
     app.use('/api/attendance', router);
--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -2,14 +2,17 @@ module.exports = app => {
   const employees = require("../handlers/employee.controller.js");
   var router = require("express").Router();
   // Create a new Tutorial
-  router.post("/", employees.create);
+  router.post("/", function(req, res){employees.create});
  
 
  
   router.get("/", employees.employeeList);
   
-  router.post('/search', employees.employeeSearch);
-  router.post('/create', employees.employeeCreate);
+  router.post('/search', function(req, res){
+    employees.employeeSearch
+  });
+  router.post('/create', function(req, res){
+    employees.employeeCreate});
   app.use('/api/employee', router);
 
   
